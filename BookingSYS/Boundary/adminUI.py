d88b0ae99d6dbc30f4c69cd5f5569bf1a9016ded
chanzs415 <chanzishen@gmail.com>
2023-04-25 17:05:42 +0800

    25/04

--- BookingSYS/Boundary/adminUI.py
+++ BookingSYS/Boundary/adminUI.py
@@ -1,6 +1,6 @@
 import sys 
 #sys.path.append(r'C:\Users\Owner\Desktop\CSIT314\bookingSys\BookingSYS\Controller')
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QStackedWidget, QGridLayout, QMessageBox
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QStackedWidget, QGridLayout, QMessageBox, QListWidget
 from PyQt5 import QtCore, QtGui, QtWidgets
 class adminUI(QWidget):
     def __init__(self, stackedWidget):
@@ -38,30 +38,41 @@ class adminUI(QWidget):
         layoutAcc = QGridLayout()
         pageAcc = QWidget()
         #Buttons
-        self.pushButton11= QPushButton("In accounts")
-        self.pushButton21= QPushButton("Manage Profiles")
+        self.labelAcc= QLabel("In accounts")
+        self.textBox1 = QListWidget()
+        self.buttonCreate= QPushButton("Create Account")
+        self.buttonDelete = QPushButton("Delete Account")
+        self.buttonEdit = QPushButton("Edit Account")
 
         #self.pushButton1.clicked.connect(self.processHist)
         #self.pushButton2.clicked.connect(self.viewHist)
         
-        layoutAcc.addWidget(self.pushButton11)
-        layoutAcc.addWidget(self.pushButton21)
+        layoutAcc.addWidget(self.textBox1,1 ,0 ,4 ,1)
+        layoutAcc.addWidget(self.labelAcc,0,0)
+        layoutAcc.addWidget(self.buttonCreate, 1 ,1)
+        layoutAcc.addWidget(self.buttonDelete, 1 ,2)
+        layoutAcc.addWidget(self.buttonEdit, 2 ,1)
         pageAcc.setLayout(layoutAcc)
 
         # layout for manage profile-----------------------------------
         layoutProf = QGridLayout()
         pageProf = QWidget()
         #Buttons
-        self.pushButton12= QPushButton("In profile")
-        self.pushButton22= QPushButton("Manage Profiles")
+        self.labelProf= QLabel("In profiles")
+        self.textBox2 = QListWidget()
+        self.buttonCreate2= QPushButton("Create Profile")
+        self.buttonDelete2 = QPushButton("Delete Profile")
+        self.buttonEdit2 = QPushButton("Edit Profile")
 
         #self.pushButton1.clicked.connect(self.processHist)
         #self.pushButton2.clicked.connect(self.viewHist)
         
-        layoutProf.addWidget(self.pushButton12)
-        layoutProf.addWidget(self.pushButton22)
+        layoutProf.addWidget(self.textBox2,1 ,0 ,4 ,1)
+        layoutProf.addWidget(self.labelProf,0,0)
+        layoutProf.addWidget(self.buttonCreate2, 1 ,1)
+        layoutProf.addWidget(self.buttonDelete2, 1 ,2)
+        layoutProf.addWidget(self.buttonEdit2, 2 ,1)
         pageProf.setLayout(layoutProf)
-
 
         # set the layout for the window to the stackedWidget
         self.stackedWidget2.addWidget(pageStart)
